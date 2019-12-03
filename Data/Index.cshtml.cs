239fe11b4da6bd7dff79c44a8cdb2f7ec5f260e1
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-12-03 12:37:34 -0600

    Add files via upload

---
+++ Data/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NewProject1.Data;
+
+namespace NewProject1.Pages.Classes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NewProject1.Data.ApplicationDbContext _context;
+
+        public IndexModel(NewProject1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Intro> Intro { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Intro = await _context.Intro.ToListAsync();
+        }
+    }
+}
